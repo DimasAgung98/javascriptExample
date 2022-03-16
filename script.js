fc68859b267b63e15433292dd4e64fea446f6bf2
DimasAgung <dimasraden0798@gmail.com>
2022-03-16 16:40:38 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var age = prompt ("You must be 21+ to view this page \nInput your age:");
+if (age >= 21) {
+    alert ("Welcome and enjoy the website")
+} 
+else {
+    alert ("you are not old enough to access this website")
+}
